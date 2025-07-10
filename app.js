8b9325aebd8e9f62722121460c707f9d26a0a0c9
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-07-10 19:44:07 +0300

    Feat: Enable CORS Requests
    Our API service will accept requests from any origin for now

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const authRouter = require('./routes/authRouter');
 const appRouter = require('./routes/appRouter');
 const adminRouter = require('./routes/adminRouter');
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res, next) => {
   res.json('Hello. This is the homepage.')
